be0f17a237b5ba107db11d7e56fec248c27fcf01
adityasingh1823 <adityasinghvk23@gmail.com>
2026-03-27 03:29:10 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,41 +1,24 @@
-import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 import Navbar from "./components/Navbar";
-import Upload from "./components/Upload";
-import Result from "./components/Result";
-import Loader from "./components/Loader";
-import { motion } from "framer-motion";
 
 function App() {
-  const [result, setResult] = useState(null);
-  const [loading, setLoading] = useState(false);
-
   return (
-    <div className="min-h-screen bg-gradient-to-br from-black via-purple-900 to-blue-900 text-white">
-      
-      <Navbar />
-
-      <div className="flex flex-col items-center justify-center mt-10 px-4">
+    <BrowserRouter>
+      <div className="min-h-screen bg-gradient-to-br from-black via-purple-900 to-blue-900 text-white">
         
-        <motion.h1
-          initial={{ opacity: 0, y: -40 }}
-          animate={{ opacity: 1, y: 0 }}
-          className="text-5xl font-extrabold mb-6 text-center"
-        >
-          👁️ DristiVision
-        </motion.h1>
+        <Navbar />
 
-        <p className="mb-6 text-center opacity-80">
-          AI Powered Real vs Fake Face Detection
-        </p>
-
-        <Upload setResult={setResult} setLoading={setLoading} />
-
-        {loading && <Loader />}
-
-        {result && <Result result={result} />}
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
 
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
